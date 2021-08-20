1a47a320fc91b3b3b0f58d920edcac31e271112a
windy <707298413@qq.com>
2021-08-20 18:35:53 +0800

    tmp ddz

---
+++ conf/ddz.lua
@@ -0,0 +1,18 @@
+local ddz = {}
+
+
+ddz.room = {
+	{lv = 1, limit = {1000, 50000}}, 				-- 初级场
+	{lv = 2, limit = {10000, 500000}}, 				-- 中级场
+	{lv = 3, limit = {500000, math.huge}}, 			-- 高级场
+}
+
+
+
+
+
+
+
+
+
+return ddz
--- service/main.lua
+++ service/main.lua
@@ -15,6 +15,14 @@ skynet.start(function ()
 	skynet.newservice("debug_console", 5555)
 
 
+	-- ddz states
+
+	wind.new("match1", {})
+	wind.new("match2", {})
+	wind.new("match3", {})
+	
+	-- end
+
 	local workers = {}
 	for i=1,conf.nworker do
 		workers[i] = skynet.newservice("worker", i)
