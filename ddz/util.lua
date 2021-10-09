b355902136e8673e22006d5f60e9b8775672121e
windy <707298413@qq.com>
2021-10-09 10:14:55 +0800

    update ddz util

--- ddz/util.lua
+++ ddz/util.lua
@@ -1,18 +1,32 @@
+-----------------------------------------------------------------
+-- 斗地主经典算法(一副牌)
+-----------------------------------------------------------------
+
 local util = {}
 
-local TYPE = {
-	dan = 1,
-	dui = 2,
-	tuple = 3,
-	sandaiyi = 4,
-	sandaiyidui = 5,
-	feiji = 6,
-	liandui = 7,
-	shunzi = 8,
-	sidaier = 9,
-	sidailiangdui = 10,
-	zhadan = 11,
-	wangzha = 12
+
+local function vv(t)
+	for i,v in ipairs(t) do
+		t[v] = v
+	end
+	return t
+end
+
+local TYPE = vv{
+	"dan",
+	"dui",
+	"tuple",
+	"sandaiyi",
+	"sandaiyidui",
+	"feiji_budai",
+	"feiji_daidan",
+	"feiji_daidui",
+	"liandui",
+	"shunzi",
+	"sidaier",
+	"sidailiangdui",
+	"zhadan",
+	"wangzha"
 }
 
 local function C(card)
@@ -23,7 +37,22 @@ local function V(card)
 	return card&0x0f
 end
 
+local function successive(list)
+	assert(#list > 1)
+	for i=2,#list do
+		if list[i] ~= list[i-1] + 1 then
+			return false
+		end
+	end
+	return true
+end
 
+local function between(n, mix, max)
+	return mix <= n and n <= max
+end
+
+
+-----------------------------------------------------------------
 util.TYPE = TYPE
 util.value = V
 util.color = C
@@ -43,6 +72,32 @@ function util.shuffle(cards)
 end
 
 
+local function cards_count(cards)
+	local count = {}			-- value: count
+	local maxc = 0 				-- max count 
+	local maxc_values = {} 		-- 达到maxc 的 value list
+
+	for _,card in ipairs(cards) do
+		local v = V(card)
+		count[v] = (count[v] or 0) + 1
+		if maxc < count[v] then
+			maxc = count[v]
+		end
+	end
+
+	for v,c in pairs(count) do
+		if c == maxc then
+			maxc_values[#maxc_values+1] = v
+		end
+	end
+
+	table.sort(maxc_values, function (a, b)
+		return a < b
+	end)
+	
+	return count, maxc, maxc_values
+end
+
 local function find_first(cc)
 	for i=1,0xf do
 		if cc[i] then
@@ -53,7 +108,7 @@ end
 
 local function is_shunzi(cc, len)
 	local first_v = find_first(cc)
-	if first_v > 0x8 then
+	if first_v+len-1 > 0xc then
 		return false
 	end
 
@@ -65,13 +120,16 @@ local function is_shunzi(cc, len)
 	return TYPE.shunzi, first_v, len
 end
 
-
 local function is_liandui(cc, len)
 	if len%2 ~= 0 then
-		return
+		return false
 	end
 	local n = len//2
 	local first_v = find_first(cc)
+	if first_v+n-1 > 0xc then
+		return false
+	end
+
 	for i=1,n do
 		if cc[first_v+i-1] ~= 2 then
 			return false
@@ -80,102 +138,234 @@ local function is_liandui(cc, len)
 	return TYPE.liandui, first_v, n
 end
 
-local function find_n(cc, number)
+local function is_sandaiyidui(cc, maxc_values)
 	for v,n in pairs(cc) do
-		if n == number then
-			return v
+		if n == 2 then
+			return TYPE.sidailiangdui, maxc_values[1]
 		end
 	end
 end
 
-
-local function count_n(cc, number)
-	local c = 0
-	for v,n in pairs(cc) do
-		if n == number then
-			c = c + 1
-		end
-	end
-	return c
-end
-
-local function is_sidaier_or_sidailiangdui(cc, len)
-	local bomb = find_n(cc, 4)
-	if not bomb then
-		return
-	end
-
-	if len == 6 then
-		return TYPE.sidaier, bomb
-	elseif len == 8 then
-		if count_n(cc, 2) == 2 then
-			return TYPE.sidailiangdui, bomb
+local function is_feiji_budai(cc, maxc_values, len)
+	local n = len//3
+	if len%3 == 0 and n == #maxc_values then
+		if successive(maxc_values) then
+			return TYPE.feiji_budai, maxc_values[1], n
 		end
 	end
 end
 
--- feiji start --------------------------------------------------
-local function onlyfeiji(cc, len)
-	if len%3 ~= 0 then
+local function is_feiji_daidan(cc, maxc_values, len)
+	if len%4 ~= 0 then
 		return
 	end
 
-	local first_v = find_first(cc)
-	local n = len/3
+	local n = len//4
+	assert(n >= 2)
 
-	if first_v+n-1 >= 0xd then
-		return
-	end
-
-	local v
-	for i=1,n do
-		v = first_v+i-1
-		if cc[v] ~= 3 then
-			return
+	local function find_first()
+		for v=1,0xb do
+			if cc[v] and cc[v] >= 3 then
+				return v
+			end
 		end
 	end
-	return true
+
+	local first_v = find_first()
+	if first_v then
+		for v=first_v+1,first_v+n-1 do
+			if not cc[v] or cc[v] < 3 then
+				return
+			end
+		end
+	end
+
+	return TYPE.feiji_daidan, first_v, n
 end
 
-local function is_feiji(cc, len)
-	if len < 6 then
+local function is_feiji_daidui(cc, maxc_values, len)
+	if len%5 ~= 0 then
 		return
 	end
-end
 
-local function is_sandaiyidui(cc, len)
-	if len == 5 and count_n(cc, 3) == 1 and count_n(cc, 2) == 1 then
-		return TYPE.sandaiyidui, find_n(cc, 3)
+	local n = len//5
+	if n == #maxc_values and successive(maxc_values) then
+		local main = {}
+		for i,v in ipairs(maxc_values) do
+			main[v] = true
+		end
+
+		for v,n in pairs(cc) do
+			if not main[v] and n ~= 2 then
+				return false
+			end
+		end
+
+		return TYPE.feiji_daidui, maxc_values[1], n
 	end
 end
 
+local feiji_funcs = {
+	is_feiji_budai,
+	is_feiji_daidan,
+	is_feiji_daidui
+}
 
-local function cards_count(cards)
-	local count = {}
-	for i,card in ipairs(cards) do
-		local v = V(card)
-		count[v] = (count[v] or 0) + 1
-	end
-	return count
-end
-
-
-local function over4type(cards, len)
-	local cc = cards_count(cards)
-	
-	local funcs = {
-		is_sandaiyidui,
-		is_feiji,
-		is_shunzi,
-		is_liandui,
-		is_sidaier_or_sidailiangdui,
-	}
-
-	for i,f in ipairs(funcs) do
-		local t, w, l = f(cc, len)
+local function is_feiji(cc, maxc_values, len)
+	for i,f in ipairs(feiji_funcs) do
+		local t, w, l = f(cc, maxc_values, len)
 		if t then
 			return t, w, l
 		end
+	end
+end
+
+local function over4type(cards, len)
+	local cc, maxc, maxc_values = cards_count(cards)
+	if maxc == 4 then 		-- 四带二, 四带两队
+		if len == 6 then
+			return TYPE.sidaier, maxc_values[1]
+		elseif len == 8 and #maxc_values == 1 then
+			for v,n in pairs(cc) do
+				if v ~= maxc_values[1] and n ~= 2 then
+					goto check_feiji
+				end
+			end
+			return TYPE.sidailiangdui, maxc_values[1]
+		end
+		::check_feiji::
+		return is_feiji(cc, maxc_values, len)
+	elseif maxc == 3 then 	-- 三带一对, 飞机不带, 飞机带单, 飞机带对
+		if len == 5 then
+			return is_sandaiyidui(cc, maxc_values)
+		else
+			return is_feiji(cc, maxc_values, len)
+		end
+	elseif maxc == 2 then
+		return is_liandui(cc, len)
+	else
+		assert(maxc == 1)
+		return is_shunzi(cc, len)
+	end
+end
+
+
+function util.gt(cards, cards2)
+	local t1, w1, len1 = util.type(cards)
+	local t2, w2, len2 = util.type(cards2)
+
+	if t1 == TYPE.wangzha then
+		return true
+	end
+
+	if t2 == TYPE.wangzha then
+		return false
+	end
+
+	if t1 == TYPE.zhadan and t2 == TYPE.zhadan then
+		return w1 > w2
+	end
+
+	if t1 == TYPE.zhadan then
+		return true
+	end
+
+	if t2 == TYPE.zhadan then
+		return false
+	end
+
+	if t1 == t2 then
+		return len1 == len2 and w1 > w2
+	end
+end
+
+-------------------------------------------------
+local display = {}
+
+local function display_sorter(a, b)
+	local v1 = V(a)
+	local v2 = V(b)
+
+	if v1 == v2 then
+		return C(a) < C(b)
+	else
+		return v1 > v2
+	end
+end
+
+local function color_sorter(a, b)
+	return C(a) < C(b)
+end
+
+
+function display.sandaiyi(cards, w, l)
+	local main = {}
+	local other = {}
+
+	for i,card in ipairs(cards) do
+		if V(card) == w then
+			table.insert(main, card)
+		else
+			table.insert(other, card)
+		end
+	end
+
+	table.sort(main, color_sorter)
+	table.sort(other, display_sorter)
+
+	for i,v in ipairs(other) do
+		table.insert(main, v)
+	end
+
+	return main
+end
+
+function display.feiji_daidan(cards, w, l)
+	local main = {}
+	local other = {}
+
+	local tmp = {}
+
+	for i,card in ipairs(cards) do
+		local v = V(card)
+		if between(v, w, w+l-1) and tmp[v] ~= 3 then
+			tmp[v] = (tmp[v] or 0) + 1
+			table.insert(main, card)
+		else
+			table.insert(other, card)
+		end
+	end
+
+	table.sort(main, display_sorter)
+	table.sort(other, display_sorter)
+
+	for i,v in ipairs(other) do
+		table.insert(main, v)
+	end
+
+	return main
+end
+
+display.sandaiyidui 	= display.sandaiyi
+display.sidaier 		= display.sandaiyi
+display.sidailiangdui 	= display.sandaiyi
+
+display.feiji_daidui 	= display.feiji_daidan
+
+function util.display_cards(cards)
+	local t, w, l = util.type(cards)
+	assert(t, "invalid cards, no type")
+
+	local f = display[t]
+	if f then
+		return f(cards, w, l)
+	else
+		local r = {}
+		for i,v in ipairs(cards) do
+			r[i] = v
+		end
+		table.sort(r, display_sorter)
+		return r
 	end
 end
 
@@ -198,7 +388,7 @@ function util.type(_cards)
 		if v1 == v2 then
 			return TYPE.dui, v1
 		else
-			if (v1 == 0x5e and v2 == 0x5f) or (v1 == 0x5f and v2 == 0x5e) then
+			if v1 == 0xe and v2 == 0xf then
 				return TYPE.wangzha
 			end
 		end
@@ -215,15 +405,17 @@ function util.type(_cards)
 		local v3 = V(cards[3])
 		local v4 = V(cards[4])
 
-		if v1 == v2 and v2 == v3 then
-			if v3 == v4 then
-				return TYPE.zhadan, v1
+		if v2 == v3 then
+			if v1 == v2 then
+				if v3 == v4 then
+					return TYPE.zhadan, v1
+				else
+					return TYPE.sandaiyi, v1
+				end
 			else
-				return TYPE.sandaiyi, v1
-			end
-		else
-			if v2 == v3 and v3 == v4 then
-				return TYPE.sandaiyi, v2
+				if v3 == v4 then
+					return TYPE.sandaiyi, v2
+				end
 			end
 		end
 	else
